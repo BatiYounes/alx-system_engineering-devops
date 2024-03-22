8ef1befbe441dce3e5b3fd50cd8cacc078329cd9
BatiYounes <younesbati007@gmail.com>
2024-03-22 12:34:57 -0700

    doing the task 0x0A config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
-# Puppet manifest to install Flask from pip3
+#!/usr/bin/pup
+#install a specific version of flas 2.1.0
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => '/usr/local/bin:/usr/bin:/bin',
-  unless      => '/usr/bin/env python3 -c "import flask; print(flask.__version__)" | grep -q "2.1.0"',
-  require     => Package['python3-pip'],
-  environment => ['PATH=/usr/local/bin:/usr/bin:/bin'],
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
