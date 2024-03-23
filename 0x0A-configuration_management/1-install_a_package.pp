d45089b43263ed4852646b7b3dd0c83d7d7b09fe
BatiYounes <younesbati007@gmail.com>
2024-03-22 22:30:04 -0700

    doing the task 0x0A config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
 #!/usr/bin/pup
-# Install flask 2.1.0
-package {'flask':
+
+# Install required dependencies
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install werkzeug==2.0.1',
+  unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.0.1"',
+}
+
+# Install Flask 2.1.0
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Exec['install_werkzeug'],
 }
