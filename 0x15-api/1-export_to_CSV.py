6928afc86685ccfa4b58432e57b2e219c57cbe70
BatiYounes <Younesbati007@gmail.com>
2024-04-22 15:40:43 -0700

    doing all the best of 0x15 - API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 """
-Export all tasks that are owned by this
-employee in the CSV format.
+Export data in CSV format
 """
 import csv
+import json
 import requests
-import sys
+from sys import argv
+
+
+def export_to_csv(user_id):
+    user_url = 'https://jsonplaceholder.typicode.com/users'
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    users = requests.get(user_url).json()
+    todos = requests.get(todo_url).json()
+
+    user_data = [user for user in users if user.get('id') == int(user_id)]
+    if not user_data:
+        return "User not found"
+
+    username = user_data[0].get('username')
+    tasks = [
+        (str(user_id), username, str(task.get('completed')), task.get('title'))
+        for task in todos if task.get('userId') == int(user_id)
+    ]
+
+    if not tasks:
+        return "No tasks found for this user"
+
+    filename = f"{user_id}.csv"
+    with open(filename, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        csv_writer.writerows(tasks)
+
+    return f"CSV exported to {filename}"
+
 
 if __name__ == "__main__":
-    fake_api = 'https://jsonplaceholder.typicode.com/'
-    employee_id = sys.argv[1]
-
-    # Fetching employee information
-    employee_response = requests.get(fake_api + "users/{}".format(employee_id))
-    employee = employee_response.json()
-    if employee_response.status_code != 200:
-        print("Error: Employee not found.")
-        sys.exit(1)
-
-    employee_username = employee.get("username")
-
-    # Writing data to CSV file
-    csv_file_name = "{}.csv".format(employee_id)
-    with open(csv_file_name, "w", newline="") as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
-                             "TASK_TITLE"])
-        
-        # Fetching todos for the employee
-        tasks_response = requests.get(fake_api + "todos",
-                                      params={"userId": employee_id})
-        if tasks_response.status_code != 200:
-            print("Error: Unable to fetch tasks.")
-            sys.exit(1)
-        tasks = tasks_response.json()
-        
-        for task in tasks:
-            csv_writer.writerow([employee_id, employee_username,
-                                str(task.get("completed")), task.get("title")])
-
-    print("Number of tasks in CSV: OK")
-    print("Data exported to {} successfully.".format(csv_file_name))
+    if len(argv) != 2:
+        print("Usage: ./1-export_to_CSV.py <user_id>")
+    else:
+        user_id = argv[1]
+        print(export_to_csv(user_id))
