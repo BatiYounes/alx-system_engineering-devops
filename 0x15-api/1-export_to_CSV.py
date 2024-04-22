e4dcb80ca3fcd9031c01517798ce6a785a944960
BatiYounes <Younesbati007@gmail.com>
2024-04-22 15:44:13 -0700

    doing all the best of 0x15 - API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 """
-export all tasks that are owned by this employee in the CSV format.
+Exports all tasks owned by a specific employee in CSV format.
+
+This script takes an employee ID as a command-line 
+argument and retrieves all tasks
+associated with that employee from a fake API 
+It then exports the tasks into a CSV file named after the employee ID.
+
+Usage: python3 script_name.py employee_id
 """
+
 if __name__ == "__main__":
     import csv
     import requests
@@ -11,8 +19,8 @@ if __name__ == "__main__":
     _id = sys.argv[1]
     employee = requests.get(fake_api + "users/{}".format(_id)).json()
     tasks = requests.get(fake_api + "todos", params={"userId": _id}).json()
-    employee_username = user.get("username")
+    employee_username = employee.get("username")
     with open("{}.csv".format(_id), "w", newline="") as csv_file:
-    csv_w = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-    [csv_w.writerow([_id, employee_username, task.get("completed"),
-                     task.get("title")]) for task in tasks]
+        csv_w = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        [csv_w.writerow([_id, employee_username, task.get("completed"),
+                         task.get("title")]) for task in tasks]
