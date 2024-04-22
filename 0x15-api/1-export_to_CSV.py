0c8625c01bd852f336f194d5752f97f0dbb07824
BatiYounes <Younesbati007@gmail.com>
2024-04-22 15:08:26 -0700

    doing all the best of 0x15 - API

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-"""
-export all tasks that are owned by this employee in the CSV format.
-"""
-if __name__ == "__main__":
-    import csv
-    import requests
-    import sys
+"""Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress and exports data in CSV format."""
+import csv
+import requests
+import sys
 
+if __name__ == "__main__":
     fake_api = 'https://jsonplaceholder.typicode.com/'
-    _id = sys.argv[1]
-    employee = requests.get(fake_api + "users/{}".format(_id)).json()
-    tasks = requests.get(fake_api + "todos", params={"userId": _id}).json()
-    employee_username = user.get("username")
-    with open("{}.csv".format(_id), "w", newline="") as csv_file:
-    csv_w = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-    [csv_w.writerow([_id, employee_username, task.get("completed"),
-                     task.get("title")]) for task in tasks]
+    employee_id = sys.argv[1]
+
+    # Fetching employee information
+    employee = requests.get(fake_api + "users/{}".format(employee_id)).json()
+    employee_name = employee.get("username")
+
+    # Fetching todos for the employee
+    todos = requests.get(fake_api + "todos", params={"userId": employee_id}).json()
+
+    # Filtering completed tasks and preparing data for CSV
+    csv_data = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
+    for todo in todos:
+        task_completed = "True" if todo.get("completed") else "False"
+        task_title = todo.get("title")
+        csv_data.append([employee_id, employee_name, task_completed, task_title])
+
+    # Writing data to CSV file
+    csv_file_name = "{}.csv".format(employee_id)
+    with open(csv_file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(csv_data)
+
+    print("Data exported to {} successfully.".format(csv_file_name))
