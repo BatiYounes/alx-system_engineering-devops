b708ee80a4327728de286ba44a56fe2ee46164dd
younesbati <younesbati007@mail.com>
2024-06-28 06:55:49 -0700

    doing the jobs /task of 0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install flask 2.1.0
-package {'flask':
+# Install flask version 2.1.0 using pip3
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
