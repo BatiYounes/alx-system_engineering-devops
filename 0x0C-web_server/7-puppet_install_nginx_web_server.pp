a1683bb8cee5d85857dbb8a180559e271b162276
batiyounes <younesbati007@gmail.com>
2024-07-02 16:47:39 -0700

    doing the task of 0x0C Web Server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,64 +1,6 @@
-# This Puppet manifest will install and configure an Nginx server
-# on an Ubuntu machine to meet the specified requirements.
-
-# Ensure the nginx package is installed
-package { 'nginx':
-  ensure => installed,
-}
-
-# Ensure the nginx service is running and enabled
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  require   => Package['nginx'],
-}
-
-# Configure the Nginx server
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Create a custom index.html file with "Hello World!" content
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-# Template for the Nginx configuration
-file { '/etc/puppetlabs/code/environments/production/modules/nginx/templates/default.erb':
-  ensure  => file,
-  content => @("EOF"),
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html;
-
-    server_name _;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 http://$server_name/;
-    }
-
-    error_page 404 /404.html;
-    location = /404.html {
-        internal;
-    }
-
-    error_page 500 502 503 504 /50x.html;
-    location = /50x.html {
-        internal;
-    }
-}
-    | EOF
+# Install Nginx web server (w/ Puppet).
+exec {
+	'install':
+  		provider => shell,
+  		command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/BatiYounes permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
