8f6ba1db6c5ee8fe79977a0919c298c234a41987
Younesbati <younesbati007@gmail.come>
2024-07-29 04:10:44 -0700

    aadding files for the api tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,40 +1,45 @@
+#!/usr/bin/python3
+"""
+This module fetches and exports to-do list data in JSON format
+for all users from a REST API.
+"""
+
 import json
 import requests
 
+
 def fetch_data():
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
-
-    users_response = requests.get(users_url)
-    todos_response = requests.get(todos_url)
-
-    users = users_response.json()
-    todos = todos_response.json()
+    """Fetch data from the API and return it as JSON."""
+    base_url = 'https://jsonplaceholder.typicode.com'
+    users = requests.get(f'{base_url}/users').json()
+    todos = requests.get(f'{base_url}/todos').json()
 
     return users, todos
 
-def process_data(users, todos):
-    data = {}
-    for user in users:
-        user_id = user['id']
-        username = user['username']
-        user_tasks = []
-        for todo in todos:
-            if todo['userId'] == user_id:
-                task_info = {
-                    "username": username,
-                    "task": todo['title'],
-                    "completed": todo['completed']
-                }
-                user_tasks.append(task_info)
-        data[user_id] = user_tasks
-    return data
 
-def export_to_json(data):
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(data, json_file, indent=4)
+def create_user_tasks(users, todos):
+    """Create a dictionary of user tasks."""
+    user_tasks = {user['id']: [] for user in users}
+    for task in todos:
+        task_data = {
+            'username': next(
+                user['username'] for user in users if user['id'] == task['userId']
+            ),
+            'task': task['title'],
+            'completed': task['completed']
+        }
+        user_tasks[task['userId']].append(task_data)
 
-if __name__ == "__main__":
+    return user_tasks
+
+
+def export_to_json(user_tasks):
+    """Export user tasks to a JSON file."""
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(user_tasks, json_file)
+
+
+if __name__ == '__main__':
     users, todos = fetch_data()
-    data = process_data(users, todos)
-    export_to_json(data)
+    user_tasks = create_user_tasks(users, todos)
+    export_to_json(user_tasks)
