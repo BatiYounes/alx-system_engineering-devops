1d5b3c62736567d122d1f4fa2aed9269b102fca8
Younesbati <younesbati007@gmail.come>
2024-07-29 04:06:20 -0700

    aadding files for the api tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,40 @@
-#!/usr/bin/python3
-"""
-export all tasks that are owned by this employee in the JSON format.
-"""
-if __name__ == "__main__":
-    import json
-    import requests
+import json
+import requests
 
-    fake_api = "https://jsonplaceholder.typicode.com/"
-    employees = requests.get(fake_api + "users").json()
+def fetch_data():
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+
+    users_response = requests.get(users_url)
+    todos_response = requests.get(todos_url)
+
+    users = users_response.json()
+    todos = todos_response.json()
+
+    return users, todos
+
+def process_data(users, todos):
+    data = {}
+    for user in users:
+        user_id = user['id']
+        username = user['username']
+        user_tasks = []
+        for todo in todos:
+            if todo['userId'] == user_id:
+                task_info = {
+                    "username": username,
+                    "task": todo['title'],
+                    "completed": todo['completed']
+                }
+                user_tasks.append(task_info)
+        data[user_id] = user_tasks
+    return data
+
+def export_to_json(data):
     with open("todo_all_employees.json", "w") as json_file:
-        json.dump({
-            user.get("id"): [{"task": task.get("title"),
-                              "completed": task.get("completed"),
-                              "username": user.get("username")
-                              } for task in requests.get(fake_api + "todos",
-                                       params={"userId": user.get("id")}
-                                       ).json()]
-            for user in users}, json_file)
+        json.dump(data, json_file, indent=4)
+
+if __name__ == "__main__":
+    users, todos = fetch_data()
+    data = process_data(users, todos)
+    export_to_json(data)
